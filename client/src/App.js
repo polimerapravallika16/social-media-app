1915d4f46bc3d125baa68e3c138cbdb9f69de2c6
pravallika <polimerapravallika16@gmail.com>
2026-05-30 22:35:31 +0530

    Fix Profile.js and BOM issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-﻿import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
